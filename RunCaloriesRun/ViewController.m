784a141074b11726f9b836cd1a23604fb12c8a90
Jonathan Tamboer <jonathanlemuel@me.com>
2012-08-17 13:02:22 -0700

    fix seconds and accuracy of location intervals

--- RunCaloriesRun/ViewController.m
+++ RunCaloriesRun/ViewController.m
@@ -13,6 +13,7 @@
 {
     double _weight;
     double _totalCalories;
+    double _totalDistance;
 }
 - (IBAction)startButtonPressed;
 - (IBAction)stopButtonPressed;
@@ -20,6 +21,7 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *weightInput;
 @property (weak, nonatomic) IBOutlet UILabel *totalCaloriesOutput;
+@property (weak, nonatomic) IBOutlet UILabel *totalDistanceOutput;
 
 @property (weak, nonatomic) IBOutlet UILabel *caloriesPerSecondOutput;
 @property (strong) CLLocationManager *locationManager;
@@ -31,6 +33,7 @@
 @synthesize startButton = _startButton;
 @synthesize weightInput = _weightInput;
 @synthesize totalCaloriesOutput = _totalCaloriesOutput;
+@synthesize totalDistanceOutput = _totalDistanceOutput;
 @synthesize caloriesPerSecondOutput = _caloriesPerSecondOutput;
 - (void)viewDidLoad
 {
@@ -38,9 +41,12 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     self.locationManager = [CLLocationManager new];
-    [self.locationManager setDelegate:self];
-    [self.locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
-    [self.weightInput setDelegate:self];
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.locationManager.distanceFilter = 10.0;
+    self.weightInput.delegate = self;
+    
+    
 }
 
 
@@ -50,6 +56,7 @@
     [self setTotalCaloriesOutput:nil];
     [self setWeightInput:nil];
     [self setStartButton:nil];
+    [self setTotalDistanceOutput:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -63,29 +70,45 @@
     
     self.caloriesPerSecondOutput.text = [NSString stringWithFormat:@"%f", caloriesPerSecond];
     self.totalCaloriesOutput.text = [NSString stringWithFormat:@"%f", _totalCalories];
+    self.totalDistanceOutput.text = [NSString stringWithFormat:@"%f", _totalDistance];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
 
     double distanceInMeters = [newLocation distanceFromLocation:oldLocation];
     double speedInMinutes = (newLocation.speed)*60;
+    double seconds = [newLocation.timestamp timeIntervalSince1970] - [oldLocation.timestamp timeIntervalSince1970];
+    if (newLocation.horizontalAccuracy > 5.0) {
+        return;
+    }
+    
+    if (speedInMinutes < 0.0) {
+        speedInMinutes = 0.0;
+    }
+    
+    if (distanceInMeters < 0.0) {
+        distanceInMeters = 0.0;
+    }
+    
+    // We even burn lots of calories going downhill! Not really right.
     double altitudeChange = fabs(newLocation.altitude - oldLocation.altitude);
     
     double caloriesPerSecond = [self calculateCaloriesPerSecondWithDistanceInMeter:distanceInMeters andSpeedInMinutes:speedInMinutes andAltitudeChange:altitudeChange];
     
-    _totalCalories += caloriesPerSecond;
+    _totalCalories += caloriesPerSecond * seconds;
+    _totalDistance += distanceInMeters;
     
     [self updateLabelsWithCaloriesPerSecond:caloriesPerSecond];
 }
 
 -(double)calculateCaloriesPerSecondWithDistanceInMeter:(double)distanceInMeters andSpeedInMinutes:(double)speedInMinutes andAltitudeChange:(double)altitudeChange
 {
-    if (!distanceInMeters) {
+    if (!distanceInMeters || !speedInMinutes) {
      return 0.0;
     }
+    
      
-     
-    double grade = (altitudeChange / distanceInMeters )*100;
+    double grade = (altitudeChange / distanceInMeters );
 
     double VO2 = 3.5 + (speedInMinutes * 0.2) + (speedInMinutes * grade * 0.9);
     double METS = VO2 / 3.5;
@@ -103,8 +126,10 @@
 - (IBAction)startButtonPressed {
     _weight = [self.weightInput.text doubleValue];
     _totalCalories = 0.0;
+    _totalDistance = 0.0;
     
     if (_weight > 0) {
+        [self.weightInput resignFirstResponder];
         [self.locationManager startUpdatingLocation];
         [self updateLabelsWithCaloriesPerSecond:0.0];
     }
