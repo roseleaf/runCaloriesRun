1bd005570751185ae915d30dfb69061d9bfe4775
Jonathan Tamboer <jonathanlemuel@me.com>
2012-08-17 11:07:08 -0700

    add weight input and calculate total calories

--- RunCaloriesRun/ViewController.m
+++ RunCaloriesRun/ViewController.m
@@ -9,13 +9,29 @@
 #import "ViewController.h"
 #import <CoreLocation/CoreLocation.h>
 
-@interface ViewController () <CLLocationManagerDelegate>
+@interface ViewController () <CLLocationManagerDelegate, UITextFieldDelegate>
+{
+    double _weight;
+    double _totalCalories;
+}
+- (IBAction)startButtonPressed;
+- (IBAction)stopButtonPressed;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+
+@property (weak, nonatomic) IBOutlet UITextField *weightInput;
+@property (weak, nonatomic) IBOutlet UILabel *totalCaloriesOutput;
+
+@property (weak, nonatomic) IBOutlet UILabel *caloriesPerSecondOutput;
 @property (strong) CLLocationManager *locationManager;
-@property (strong) NSNumber *weight;
+-(void)updateLabelsWithCaloriesPerSecond:(double)caloriesPerSecond;
 -(double)calculateCaloriesPerSecondWithDistanceInMeter:(double)distanceInMeters andSpeedInMinutes:(double)speedInMinutes andAltitudeChange:(double)altitudeChange;
 @end
 
 @implementation ViewController 
+@synthesize startButton = _startButton;
+@synthesize weightInput = _weightInput;
+@synthesize totalCaloriesOutput = _totalCaloriesOutput;
+@synthesize caloriesPerSecondOutput = _caloriesPerSecondOutput;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -24,12 +40,16 @@
     self.locationManager = [CLLocationManager new];
     [self.locationManager setDelegate:self];
     [self.locationManager setDesiredAccuracy:kCLLocationAccuracyBest];
-    [self.locationManager startUpdatingLocation];
+    [self.weightInput setDelegate:self];
 }
 
 
 - (void)viewDidUnload
 {
+    [self setCaloriesPerSecondOutput:nil];
+    [self setTotalCaloriesOutput:nil];
+    [self setWeightInput:nil];
+    [self setStartButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -37,6 +57,12 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (void)updateLabelsWithCaloriesPerSecond:(double)caloriesPerSecond {
+    
+    self.caloriesPerSecondOutput.text = [NSString stringWithFormat:@"%f", caloriesPerSecond];
+    self.totalCaloriesOutput.text = [NSString stringWithFormat:@"%f", _totalCalories];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
@@ -47,7 +73,9 @@
     
     double caloriesPerSecond = [self calculateCaloriesPerSecondWithDistanceInMeter:distanceInMeters andSpeedInMinutes:speedInMinutes andAltitudeChange:altitudeChange];
     
-    NSLog(@"This is it: %f", caloriesPerSecond);
+    _totalCalories += caloriesPerSecond;
+    
+    [self updateLabelsWithCaloriesPerSecond:caloriesPerSecond];
 }
 
 -(double)calculateCaloriesPerSecondWithDistanceInMeter:(double)distanceInMeters andSpeedInMinutes:(double)speedInMinutes andAltitudeChange:(double)altitudeChange
@@ -61,10 +89,28 @@
 
     double VO2 = 3.5 + (speedInMinutes * 0.2) + (speedInMinutes * grade * 0.9);
     double METS = VO2 / 3.5;
-    double caloriesPerSecond = (METS * [self.weight doubleValue])/(60*60);
+    double caloriesPerSecond = (METS * _weight)/(60*60);
 
     return caloriesPerSecond;
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [self.weightInput resignFirstResponder];
+    return NO;
+}
 
+
+- (IBAction)startButtonPressed {
+    _weight = [self.weightInput.text doubleValue];
+    _totalCalories = 0.0;
+    
+    if (_weight > 0) {
+        [self.locationManager startUpdatingLocation];
+        [self updateLabelsWithCaloriesPerSecond:0.0];
+    }
+}
+
+- (IBAction)stopButtonPressed {
+    [self.locationManager stopUpdatingLocation];
+}
 @end
